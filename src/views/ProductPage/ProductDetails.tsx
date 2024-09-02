3b0bb371692dd19651f7b47019b692e5adf971f5
Paavan97 <paavantrivedi97@gmail.com>
2024-09-02 22:40:23 +0530

    Gitlab Repo Added

--- src/views/ProductPage/ProductDetails.tsx
+++ src/views/ProductPage/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Bottom from "/images/Top.png";
 import Top from "/images/bottom.png";
 import {
